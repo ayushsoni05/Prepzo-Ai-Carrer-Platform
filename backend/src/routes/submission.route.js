ef2979544268912ffa25f3484654c2eaacc0886f
Ayush Soni <aayushsonisoni58@gmail.com>
2026-05-22 23:05:53 +0530

    fix: correct typo in auth.middleware import path

--- backend/src/routes/submission.route.js
+++ backend/src/routes/submission.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
   createSubmission,
   getProblemSubmissions,
